fd50f4896d414c5ea638daaa150d96c218c1ad48
purple-beastie <purple.beastie@tuta.io>
2017-07-22 02:10:57 +0000

    Create eSIT.meta.js

---
+++ eSIT.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         eSIT: eSix Informative Thumbnails
+// @namespace    prplbst
+// @version      1.0.0
+// @description  Gives each Video, Flash, and Blacklisted thumbnail on e621.net a unique appearance while also adding helpful info overlays to them.
+// @author       purple.beastie
+// @match        https://e621.net/*
+// @match        https://e926.net/*
+// @exclude      *.json
+// @exclude      *.xml
+// @grant        none
+// @require      https://cdn.jsdelivr.net/npm/jdenticon@1.6.0/dist/jdenticon.min.js
+// ==/UserScript==
