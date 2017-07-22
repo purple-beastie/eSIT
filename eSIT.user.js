c65e9d28a5734c1b1488e56089b00e287e41056e
purple-beastie <purple.beastie@tuta.io>
2017-07-22 01:13:06 +0000

    Fix link button not showing on focus

--- eSIT.user.js
+++ eSIT.user.js
@@ -106,7 +106,7 @@
         // Blacklist link
         '.esit-blacklist-link {width:100%;height:30px;position:absolute;bottom:0;left:0;z-index:2;background:#333;display:flex;justify-content:center;align-items:center;font-size:15px;transition:opacity 0.35s ease 0.2s,background-color 0.3s ease;opacity:0;border-top-left-radius:inherit;border-top-right-radius:inherit;}',
         'a:link .esit-blacklist-link, a:visited .esit-blacklist-link {color:white}',
-        '.esit-blacklist-link:hover, .esit-blacklist-link:focus {opacity:1;color:inherit;}',
+        '.esit-blacklist-link:hover, a:focus .esit-blacklist-link {opacity:1;color:inherit;}',
         '.esit-blacklist-link:active {background:#555;}',
 
         '.esit-preview-bg {background:hsla(0,0%,100%,0.12) !important}',
