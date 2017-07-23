878964cb2f82e1928117c0d48dec949ce68077a7
purple-beastie <purple.beastie@tuta.io>
2017-07-23 04:33:33 +0000

    Change how link is styled when focused and hovered
    Now matches :active styling

--- eSIT.user.js
+++ eSIT.user.js
@@ -106,9 +106,8 @@
         // Blacklist link
         '.esit-blacklist-link {width:100%;height:30px;position:absolute;bottom:0;left:0;z-index:2;background:#333;display:flex;justify-content:center;align-items:center;font-size:15px;transition:opacity 0.35s ease 0.2s,background-color 0.3s ease;opacity:0;border-top-left-radius:inherit;border-top-right-radius:inherit;}',
         'a:link .esit-blacklist-link, a:visited .esit-blacklist-link, a:focus .esit-blacklist-link {color:white}',
-        'a:focus .esit-blacklist-link:not(:active):hover {text-decoration:underline}',
         '.esit-blacklist-link:hover, a:focus .esit-blacklist-link {opacity:1}',
-        '.esit-blacklist-link:active {background:#555;}',
+        '.esit-blacklist-link:active, a:focus .esit-blacklist-link:hover {background:#555}',
 
         '.esit-preview-bg {background:hsla(0,0%,100%,0.12) !important}',
         '.esit-preview-border {width: 100%;height: 100%;box-sizing: border-box;border: 1px #B00 solid;position: absolute;z-index:3;left: 0;top: 0;border-radius: inherit;pointer-events:none;}',
