fabec4ec96d9bcde20ea857353e2b5499d1dae0c
purple-beastie <purple.beastie@tuta.io>
2017-07-23 04:46:40 +0000

    Add updateURL

--- eSIT.user.js
+++ eSIT.user.js
@@ -4,6 +4,7 @@
 // @version      1.0.0
 // @description  Gives each Video, Flash, and Blacklisted thumbnail on e621.net a unique appearance while also adding helpful info overlays to them.
 // @author       purple.beastie
+// @updateURL    https://raw.githubusercontent.com/purple-beastie/eSIT/master/eSIT.meta.js
 // @match        https://e621.net/*
 // @match        https://e926.net/*
 // @exclude      *.json
